91f90b3d7030e0994fe417b7ef5c3b8dc6b77b18
SPraveeen <praveenji8498@gmail.com>
2026-06-23 19:33:43 +0530

    get-second-largest

---
+++ get_second_laregst.py
@@ -0,0 +1,17 @@
+arr=[10,30,20,40,60,80,50,70]
+a=sorted(arr)
+# print(a[-2])
+#  but the above time complexity is high
+
+def get_second_largest(arr):
+    first_big=1
+    second_big=0
+    for i in arr:
+        if i>first_big:
+            second_big=first_big
+            first_big=i
+        elif i>second_big:
+            second_big=i
+    return first_big,second_big
+
+print(get_second_largest(arr))
